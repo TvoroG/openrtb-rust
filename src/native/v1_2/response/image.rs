7537e16baf662daf4c48f3374c65c4e4eec1ba9c
TvoroG <marszaripov@gmail.com>
2021-02-12 19:27:52 +0300

    fix native response image url

--- src/native/v1_2/response/image.rs
+++ src/native/v1_2/response/image.rs
@@ -2,8 +2,7 @@ use serde_utils;
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct Image {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub url: Option<String>,
+    pub url: String,
 
     #[serde(rename = "w", skip_serializing_if = "Option::is_none")]
     pub width: Option<u32>,
